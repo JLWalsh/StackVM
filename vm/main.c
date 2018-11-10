238943058c3dc8a999143c1c0fbbaf6200106031
James L. Walsh <jloubierwalsh@gmail.com>
2018-11-10 15:53:39 -0500

    Fixed bug in test program

--- vm/main.c
+++ vm/main.c
@@ -20,16 +20,16 @@ int main(int argc, char const* argv[])
     // };
 
     char d[] = {
-        OP_PUSH, 5, 0, // PUSH
-        OP_CALL, 10, 0, 1, 0,
-        OP_PUSH, 0, 0,
-        OP_I_AND,
+        OP_I_PUSH, 5, 0, // PUSH
+        OP_CALL, 14, 0, 1, 0,
+        OP_I_PUSH, 27, 0,
+        OP_I_OR,
         OP_PRINT,
         OP_HALT,
         OP_LOADARG, 0, 0,
-        OP_PUSH, 2, 0,
+        OP_I_PUSH, 2, 0,
         OP_ADD,
-        OP_PUSH, 4, 0,
+        OP_I_PUSH, 4, 0,
         OP_CALL, 30, 0, 2, 0,
         OP_RETURN,
         OP_LOADARG, 0, 0,
