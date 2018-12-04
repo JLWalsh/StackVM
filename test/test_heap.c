cd0051ec0c92d7cc3bb4c2f6dd719d887a4bddf8
James L Walsh <jloubierwalsh@gmail.com>
2018-12-03 19:40:53 -0500

    Started testing the heap

--- test/test_heap.c
+++ test/test_heap.c
@@ -1,12 +1,28 @@
 #include <unity.h>
+#include <vm/constants.h>
 #include <vm/heap.h>
 
-void one_equals_one()
+static char FILL_CONSTANTS[]      = { 1, 2, 3, 4 };
+static int  FILL_CONSTANTS_LENGTH = 4;
+
+void test_heap_from_constants_should_include_constants()
 {
-    TEST_ASSERT(1 == 1);
+    CONSTANTS constants;
+    constants.data   = &FILL_CONSTANTS;
+    constants.length = FILL_CONSTANTS_LENGTH;
+
+    HEAP  heap              = heap_from(constants, 0);
+    char* constants_in_heap = (char*)heap_at(&heap, 0);
+
+    TEST_ASSERT_EQUAL_HEX8(FILL_CONSTANTS[0], constants_in_heap[0]);
+    TEST_ASSERT_EQUAL_HEX8(FILL_CONSTANTS[1], constants_in_heap[1]);
+    TEST_ASSERT_EQUAL_HEX8(FILL_CONSTANTS[2], constants_in_heap[2]);
+    TEST_ASSERT_EQUAL_HEX8(FILL_CONSTANTS[3], constants_in_heap[3]);
 }
 
-int main(int argc, char const* argv[])
+int main(void)
 {
-    return 1;
+    UNITY_BEGIN();
+    RUN_TEST(test_heap_from_constants_should_include_constants);
+    return UNITY_END();
 }
