c50cc79534f38206d93f3de345ef20e3a23d9b20
James L. Walsh <jloubierwalsh@gmail.com>
2018-12-14 21:24:03 -0500

    Fix executable test

--- test/test_executable.c
+++ test/test_executable.c
@@ -9,8 +9,8 @@ char stream_advance_char(STREAM* program)
 void test_executable_header_should_parse_executable_length()
 {
     char executable[] = {
-        0, 0, 0, 0, 0, 0, 12, // Executable length
-        0, 0, 0, 0, 0, 0, 0, // Constants length
+        0, 0, 0, 0, 0, 0, 0, 12, // Executable length
+        0, 0, 0, 0, 0, 0, 0, 0, // Constants length
         0 // Program
     };
     STREAM s = stream_create(&executable);
@@ -23,8 +23,8 @@ void test_executable_header_should_parse_executable_length()
 void test_executable_header_should_parse_constants_length()
 {
     char executable[] = {
-        0, 0, 0, 0, 0, 0, 12, // Executable length
-        0, 0, 0, 0, 0, 0, 241, // Constants length
+        0, 0, 0, 0, 0, 0, 0, 12, // Executable length
+        0, 0, 0, 0, 0, 0, 0, 241, // Constants length
         0 // Program
     };
     STREAM s = stream_create(&executable);
@@ -37,20 +37,20 @@ void test_executable_header_should_parse_constants_length()
 void test_executable_should_copy_constants()
 {
     char executable[] = {
-        0, 0, 0, 0, 0, 0, 21, // Executable length
-        0, 0, 0, 0, 0, 0, 4, // Constants length
+        0, 0, 0, 0, 0, 0, 0, 21, // Executable length
+        0, 0, 0, 0, 0, 0, 0, 4, // Constants length
         1, 2, 3, 4, // Constants
         0 // Program
     };
 
     EXECUTABLE parsed_executable = executable_from(&executable);
-    char*      parsed_constants  = (char*)parsed_executable.constants.data;
+    char*      parsed_constants  = (char*)parsed_executable.constants;
 
     TEST_ASSERT_EQUAL_UINT8(1, parsed_constants[0]);
     TEST_ASSERT_EQUAL_UINT8(2, parsed_constants[1]);
     TEST_ASSERT_EQUAL_UINT8(3, parsed_constants[2]);
     TEST_ASSERT_EQUAL_UINT8(4, parsed_constants[3]);
-    TEST_ASSERT_EQUAL_UINT64(parsed_executable.constants.length, 4);
+    TEST_ASSERT_EQUAL_UINT64(parsed_executable.constants_length, 4);
 
     executable_free(parsed_executable);
 }
@@ -58,8 +58,8 @@ void test_executable_should_copy_constants()
 void test_executable_should_copy_program()
 {
     char executable[] = {
-        0, 0, 0, 0, 0, 0, 20, // Executable length
-        0, 0, 0, 0, 0, 0, 0, // Constants length
+        0, 0, 0, 0, 0, 0, 0, 20, // Executable length
+        0, 0, 0, 0, 0, 0, 0, 0, // Constants length
         10, 20, 30, 40 // Program
     };
 
@@ -79,7 +79,7 @@ int main(void)
     RUN_TEST(test_executable_header_should_parse_constants_length);
     RUN_TEST(test_executable_header_should_parse_executable_length);
     RUN_TEST(test_executable_should_copy_constants);
-    // RUN_TEST(test_executable_should_copy_program);
+    RUN_TEST(test_executable_should_copy_program);
 
     return UNITY_END();
 }
